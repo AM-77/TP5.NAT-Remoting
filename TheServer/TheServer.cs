8986c21f91457910364a7a67fd3cf7349af84a7a
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-12-09 23:08:29 +0100

    Added TheServer class.

---
+++ TheServer/TheServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using StackFactory;
+
+namespace TheServer
+{
+    class TheServer
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TcpChannel tcpChannel = new TcpChannel(1234);
+                ChannelServices.RegisterChannel(tcpChannel, false);
+
+                RemotingConfiguration.RegisterWellKnownServiceType(typeof(StackFactory.StackFactory), "uPile", WellKnownObjectMode.Singleton);
+
+                Console.Write("[+] The server is running ...  \n[INFO] Rememeber to activate win32 in .bashrc");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.Write("[!] There was an error in the serverMain : {0}", e.Message);
+            }
+        }
+    }
+}
