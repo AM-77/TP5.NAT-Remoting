d2a03de3aa982d14510405908a522f767279b0f5
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-12-09 23:34:44 +0100

    Added the .NET Remoting figlet text.

--- TheServer/TheServer.cs
+++ TheServer/TheServer.cs
@@ -14,7 +14,10 @@ namespace TheServer
                 TcpChannel tcpChannel = new TcpChannel(1234);
                 ChannelServices.RegisterChannel(tcpChannel, false);
 
-                RemotingConfiguration.RegisterWellKnownServiceType(typeof(StackFactory.StackFactory), "uPile", WellKnownObjectMode.Singleton);
+                RemotingConfiguration.RegisterWellKnownServiceType(typeof(StackFactory.StackFactory), "StackFactory", WellKnownObjectMode.Singleton);
+
+                String netremoting = "   _   _ _____ _____   ____                      _   _             \n  | \\ | | ____|_   _| |  _ \\ ___ _ __ ___   ___ | |_(_)_ __   __ _ \n  |  \\| |  _|   | |   | |_) / _ \\ '_ ` _ \\ / _ \\| __| | '_ \\ / _` |\n _| |\\  | |___  | |   |  _ <  __/ | | | | | (_) | |_| | | | | (_| |\n(_)_| \\_|_____| |_|   |_| \\_\\___|_| |_| |_|\\___/ \\__|_|_| |_|\\__, |\n                                                             |___/ ";
+                Console.WriteLine(netremoting);
 
                 Console.Write("[+] The server is running ...  \n[INFO] Rememeber to activate win32 in .bashrc");
                 Console.ReadLine();
