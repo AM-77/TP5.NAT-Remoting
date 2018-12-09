08025db0def63f36eb894c43aa033596c45e4509
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-12-09 23:00:14 +0100

    Added the StackFactory implementation.

--- StackFactory/StackFactory.cs
+++ StackFactory/StackFactory.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 namespace StackFactory
 {
-    public class StackFactory
+    public class StackFactory : MarshalByRefObject, IStackFactory
     {
         public StackFactory()
         {
+        }
+
+        public Stack CreateStack(int size)
+        {
+            return new Stack(size);
         }
     }
 }
