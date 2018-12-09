fad7e74bed8760712e26761c83caa77725090ed1
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-12-09 22:49:45 +0100

    Added the Student Class.

--- StackFactory/IStack.cs
+++ StackFactory/IStack.cs
@@ -3,5 +3,6 @@ namespace StackFactory
 {
     public interface IStack
     {
+
     }
 }
---
+++ StackFactory/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StackFactory
+{
+    [Serializable]
+    public class Student
+    {
+
+        private int id { set; get; }
+        private String name { set; get; }
+        private String f_name { set; get; }
+
+        public Student(int id, String name, String f_name)
+        {
+            this.id = id;
+            this.name = name;
+            this.f_name = f_name;
+        }
+    }
+}
