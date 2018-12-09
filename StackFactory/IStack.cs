dbffe1b05c39c00b9a5edbe459d25e08ac128258
Mohamed Amine Griche <amine.griche77@gmail.com>
2018-12-09 22:51:58 +0100

    Added the IStack interface.

--- StackFactory/IStack.cs
+++ StackFactory/IStack.cs
@@ -3,6 +3,10 @@ namespace StackFactory
 {
     public interface IStack
     {
-
+        bool Push(Student s);
+        Student Pop();
+        void Empty();
+        int Size();
+        int MaxSize();
     }
 }
